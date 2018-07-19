550c5af7bd9bba6e092806c054b540a95b2421c7
Prashanth <prashanthh727@gmail.com>
2018-07-20 00:01:24 +0530

    Initial Commit

---
+++ src/main/java/com/milkyway/SpringUserREST/SpringUserRestApplication.java
@@ -0,0 +1,12 @@
+package com.milkyway.SpringUserREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUserRestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/milkyway/SpringUserREST/UserController.java
@@ -0,0 +1,26 @@
+package com.milkyway.SpringUserREST;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+	@GetMapping("/greeting/{name}")
+	public Greeting getGreeting(@PathVariable(value="name",required=false) String name) {
+		return new Greeting(counter.incrementAndGet(),
+				String.format(template, name));
+	}
+}
